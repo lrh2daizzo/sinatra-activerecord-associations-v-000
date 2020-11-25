50ab38794fff9af0f13045237d9a0f2d9184007f
Luis Herrera <herrera.lrh@gmail.com>
2020-11-25 04:20:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201125041309) do
+ActiveRecord::Schema.define(version: 20201125041750) do
 
   create_table "cats", force: :cascade do |t|
     t.string  "name"
     t.integer "age"
     t.string  "breed"
+    t.integer "owner_id"
   end
 
   create_table "owners", force: :cascade do |t|
